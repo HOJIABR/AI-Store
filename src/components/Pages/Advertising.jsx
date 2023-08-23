034108a1bae7c6f8b1bee1e4903f889c6e1f58fe
Xojiakbar <hojiakbarsharifjonov54@gmail.com>
2023-08-23 09:39:06 +0500

    commit9

--- src/components/Pages/Advertising.jsx
+++ src/components/Pages/Advertising.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "../Header";
+import hend from "../../assets/img/svg/hend.svg";
 
 export default function Advertising() {
   return (
@@ -11,6 +12,20 @@ export default function Advertising() {
             {/* strelkali novigatsiya boladi iinobatga o'ling!!!! */}
             <span className="navigation__strel">Главная -- Реклама</span>
             <p>Реклама</p>
+          </div>
+        </div>
+        <div className="wrapper__contact">
+          <p className="education__text">
+            Если у вас есть продукт или услуга, которую можно продвигать через
+            рекламу
+          </p>
+          <p className="education__text--end">
+            Напиши нам о своём курсе и мы разместим у себя на сайте
+          </p>
+          <div className="wrapper__telegram--coll">
+            <div className="bacgraund__telegram--inner">
+              <img src={hend} alt="" />
+            </div>
           </div>
         </div>
       </div>
